a3220fd0e4f452ebc5c887f28dc4db32ce218313
aleksandra-kim <aleksandra.kim@psi.ch>
2019-06-14 09:23:01 +0200

    refactoring by Chris

--- saindices/sobol_indices.py
+++ saindices/sobol_indices.py
@@ -1,8 +1,8 @@
 '''
 Functions that run Monte Carlo (MC) simulations and compute Sobol indices
-Input: Sampler class that can generate samples (inputs) for the model, 
-        samples can be generated one at a time using 'next' and all simultaneously as a matrix of size n_samples x n_dimensions, 
-        where n_samples is the # of MC runs and n_dimensions - # of dimensions
+Input: Sampler class that can generate samples (inputs) for the model,
+        samples can be generated one at a time using 'next' and all simultaneously as a matrix of size NxD,
+        where N is the # of MC runs and D - # of inputs
        Model, such that output = Model(sample)
 '''
 
@@ -11,102 +11,136 @@ import numpy as np
 import time
 
 
-#Compute Sobol indices while generating samples separately 
-def sobol_indices_one(Sampler,Model):
+#Compute Sobol indices while generating samples separately
+def sobol_indices_one(Samp,Model):
+    """C: Every function should have a docstring (like this) which tells what it does (if not clear from the name), the inputs arguments, and what the function returns."""
 
-	n_samples    = Sampler.n_samples
-	n_dimensions = Sampler.n_dimensions
-	n_samples_2  = n_samples//2
+    """C: Can we say Sampler instead of Samp?"""
 
-	next(Sampler) #TODO change later, needed because of a bug
+	#we generate N number of samples, actual number of MC runs is N//2
 
-	y_A = [0]*n_samples_2
-	y_B = [0]*n_samples_2
-	y_j = np.zeros([n_samples_2,n_dimensions])
+    """C: Can't we just use Samp.N?"""
+	N = Samp.N
+	D = Samp.D
 
-	total_index = [0]*n_dimensions
-	first_index = [0]*n_dimensions
+    """C: Note that this will round down. May not be desired behaviour."""
 
-	for i in range(n_samples_2): #compute total indices for all parameters
+    nruns = N//2
 
-		sample_A = next(Sampler)
-		sample_B = next(Sampler)
+	next(Samp) #TODO change later, some bug
+
+    """C: Consider using numpy arrays here, e.g. np.zeros(nruns). Much better
+    performance for numerical stuff plus broadcasting."""
+
+    """C: Ideally these would be defined in the docstring"""
+
+	y_A = [0]*nruns
+	y_B = [0]*nruns
+	y_j = np.zeros([nruns,D])
+
+	total_index = [0]*D
+	first_index = [0]*D
+
+    """C: Could also be:
+
+    for i, sample_A, sample_B in zip(range(nruns), Samp, Samp):
+
+    """
+
+	for i in range(nruns): #compute total indices for all parameters
+
+		sample_A = next(Samp)
+		sample_B = next(Samp)
+
+        """C: Need more documentation on what is returned here (vector? float?)"""
 
 		y_A[i]   = Model(sample_A)
 		y_B[i]   = Model(sample_B)
 
-		for j in range(n_dimensions):
+		for j in range(D):
+
+            """C: Maybe need to make a copy?"""
 
 			sample_j = sample_A
 			sample_j[j] = sample_B[j]
 			y_j[i][j] = Model(sample_j)
 
-	for j in range(n_dimensions):
-		total_index[j] = [(y_A[i]-y_j[i,j])**2 for i in range(n_samples_2)]
-		total_index[j] = sum(total_index[j])/2/n_samples_2
+	for j in range(D):
 
-		first_index[j] = [y_B[i]*(y_j[i,j]-y_A[i]) for i in range(n_samples_2)]
-		first_index[j] = sum(first_index[j])/n_samples_2
+        """C: This would be much faster if they were numpy arrays"""
+
+        total_index[j] = [(y_A[i]-y_j[i,j])**2 for i in range(nruns)]
+
+        """C: Normally would have some space here, e.g. x / y * z"""
+
+		total_index[j] = sum(total_index[j])/2/nruns
+
+		first_index[j] = [y_B[i]*(y_j[i,j]-y_A[i]) for i in range(nruns)]
+		first_index[j] = sum(first_index[j])/nruns
 
 	return first_index,total_index
 
 
 #Compute Sobol indices while generating all samples simultaneously
-def sobol_indices_all(Sampler,Model):
+def sobol_indices_all(Samp,Model):
 
-    n_samples    = Sampler.n_samples
-    n_dimensions = Sampler.n_dimensions
-    n_samples_2  = n_samples//2
+	#we generate N number of samples, actual number of MC runs is N//2
+    N = Samp.N
+    D = Samp.D
+    nruns = N//2
 
-    samples = Sampler.generate_all_samples()
+    samples = Samp.generate_all_samples() #change later, there's a bug
 
-    y_A = [0]*n_samples_2
-    y_B = [0]*n_samples_2
-    y_j = [0]*n_samples_2
+    y_A = [0]*nruns
+    y_B = [0]*nruns
+    y_j = [0]*nruns
 
-    total_index = [0]*n_dimensions
-    first_index = [0]*n_dimensions
-    
-    A = samples[:n_samples_2]
-    B = samples[n_samples_2:]
+    total_index = [0]*D
+    first_index = [0]*D
 
-    for i in range(n_samples_2):
+    A = samples[:nruns]
+    B = samples[nruns:]
+
+    for i in range(nruns):
         y_A[i] = Model(A[i])
         y_B[i] = Model(B[i])
 
-    for j in range(n_dimensions): #compute total indices for all parameters
-        
+    for j in range(D): #compute total indices for all parameters
+
         J = A
         J[:,j] = B[:,j]
 
-        for i in range(n_samples_2):
+        for i in range(nruns):
             y_j[i] = Model(J[i])
 
-        total_index[j] = [(y_A[k]-y_j[k])**2 for k in range(n_samples_2)]
-        total_index[j] = sum(total_index[j])/2/n_samples_2
+        total_index[j] = [(y_A[k]-y_j[k])**2 for k in range(nruns)]
+        total_index[j] = sum(total_index[j])/2/nruns
 
-        first_index[j] = [y_B[k]*(y_j[k]-y_A[k]) for k in range(n_samples_2)]
-        first_index[j] = sum(first_index[j])/n_samples_2 
-            
+        first_index[j] = [y_B[k]*(y_j[k]-y_A[k]) for k in range(nruns)]
+        first_index[j] = sum(first_index[j])/nruns
+
     return first_index,total_index
 
 
-#wrapper function that chooses how to calculate sobol indices depending on the problem
+#wrapper function that chooses how to calculate sobol indices depending on the problem at stake
 #for now, if problem has <1000 dimensions, generate all samples simultaneously. Otherwise, one sample at a time
-def sobol_indices(n_samples,n_dimensions,Model,Sampler=None):
+def sobol_indices(N,D,Model,Samp=None):
 
-	if n_dimensions < 1000: 
-		if Sampler==None:
-			Sampler = SobolSample(n_samples*2,n_dimensions)
+	if D < 5000:
+
+        """C: if Samp is None"""
+
+        if Samp==None:
+			Samp = SobolSample(N*2,D)
 		t0 = time.time()
-		first_index,total_index = sobol_indices_all(Sampler,Model)
+		first_index,total_index = sobol_indices_all(Samp,Model)
 		t = time.time()-t0
 		return t
-	else: 
-		if Sampler==None:
-			Sampler = SobolSample(n_samples*2+1,n_dimensions) #TODO +1 occurs due to a bug
+	else:
+		if Samp==None:
+			Samp = SobolSample(N*2+1,D) #TODO +1 occurs due to a bug
 		t0 = time.time()
-		first_index,total_index = sobol_indices_one(Sampler,Model)
+		first_index,total_index = sobol_indices_one(Samp,Model)
 		t = time.time()-t0
 		return t
 
--- sampling/sobol_sequence.py
+++ sampling/sobol_sequence.py
@@ -2,58 +2,141 @@
 Module that generates quasi-random Sobol sequences. They are needed for better space coverage.
 '''
 
+"""C: Can we have tests for expected values (e.g. from sampler)?"""
+
+"""C: Better to load/save directions.py as numpy array."""
+
+
 import numpy as np
 import math
 import sys
 
 #local files
+
+"""C: from .directions import directions"""
+
 from . import directions
+
+"""C: Delete, no need for py2 compatibility"""
 
 if sys.version_info[0] > 2:
     long = int
 
 #Class that generates Sobol sequences
 class SobolSample:
-    
-    def __init__(self,n_samples,n_dimensions,scale=31):
 
-        if n_dimensions > len(directions.directions) + 1:
+    global scale
+    scale = 31
+
+    def __init__(self,N,D):
+
+        """C: if len(directions.directions) < D:"""
+
+        if D > len(directions.directions) + 1:
             raise ValueError("Error in Sobol sequence: not enough dimensions")
 
-        L = int(math.ceil(math.log(n_samples) / math.log(2)))
+        L = int(math.ceil(math.log(N) / math.log(2)))
 
         if L > scale:
             raise ValueError("Error in Sobol sequence: not enough bits")
 
-        self.n_samples.   = n_samples
-        self.n_dimensions = n_dimensions
-        self.scale = scale
+        """Can do multiple assignment at once, e.g.
+
+        self.N, self.D, self.L = N, D, L
+
+        """
+
+        self.N = N
+        self.D = D
         self.L = L
         self.current = 0
-        self.Y = np.array([long(0)]*self.n_dimensions)
+        self.Y = np.array([long(0)]*self.D)
         self.V = self.generate_V()
-           
+
     def index_of_least_significant_zero_bit(self,value):
         index = 1
         while((value & 1) != 0):
             value >>= 1
             index += 1
         return index
-        
+
     def generate_V(self):
 
-        n_samples    = self.n_samples
-        n_dimensions = self.n_dimensions    
-        L = self.L    
+        N = self.N
+        D = self.D
+        L = self.L
 
-        V = np.zeros([L+1,n_dimensions], dtype=long)
+        """C: dtype = int"""
+
+        V = np.zeros([L+1,D], dtype=long)
+        """C: First element already zero.
+
+        V[1:,0] = [1 << (scale - j) for j in range(1, L + 1)]
+
+        """
         V[:,0] = [0]+[1 << (scale - j) for j in range(1, L + 1)]
-        
-        for i in range(1,n_dimensions):
+
+        """C: ???
+
+        for i in range(D):
+            m = np.array(directions.directions[i], dtype=int)
+
+        """
+
+        """C: Faster to already have as array:
+
+        In [1]: from directions import directions
+           ...:
+
+        In [2]: import numpy as np
+           ...:
+
+        In [3]: a = np.zeros((len(directions), max([len(row) for row in directions])))
+           ...:
+
+        In [4]: for i, row in enumerate(directions):
+           ...:     a[i, :len(row)] = row
+           ...:
+
+        In [5]: def get_with_mask(array):
+           ...:     for row in array:
+           ...:         return row[row > 0]
+           ...:
+
+        In [6]: def get_with_count(array):
+           ...:     for row in array:
+           ...:         return row[:np.count_nonzero(row)]
+           ...:
+
+        In [7]: def convert(lst):
+           ...:     for row in lst:
+           ...:         np.array(row, dtype=int)
+           ...:
+
+        In [8]: %timeit get_with_mask(a)
+           ...:
+        1.95 µs ± 51.6 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+
+        In [9]: %timeit convert(directions)
+           ...:
+        49 ms ± 3.04 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
+
+        In [10]: %timeit get_with_count(a)
+            ...:
+        792 ns ± 18.6 ns per loop (mean ± std. dev. of 7 runs, 1000000 loops each)
+
+        """
+
+        for i in range(1,D):
             m = np.array(directions.directions[i - 1], dtype=int)
+
+            """C: Maybe easier to use these value directly instead of creating
+            new variables. If you really want new variables, give them names
+            people can understand."""
+
             a = m[0]
             s = len(m) - 1
-            
+
             # The following code discards the first row of the ``m`` array
             # Because it has floating point errors, e.g. values of 2.24e-314
             if L <= s:
@@ -65,20 +148,23 @@ class SobolSample:
                     for k in range(1, s):
                         V[j,i] ^= ((a >> (s - 1 - k)) & 1) * V[j-k][i]
         return V
-    
+
     def generate_sample(self):
-        sample_one = np.zeros(self.n_dimensions)
-        for i in range(self.n_dimensions):
+        sample_one = np.zeros(self.D)
+
+        """C: Huge gains here from using numpy functions at once instead of a Python loop"""
+
+        for i in range(self.D):
             self.Y[i] ^= self.V[self.index_of_least_significant_zero_bit(self.current - 1),i]
             sample_one[i] = float(self.Y[i] / math.pow(2, scale))
         self.current += 1
         return sample_one
-    
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
-        if self.current > self.n_samples-1:
+        if self.current > self.N-1:
             raise StopIteration
         elif self.current == 0:
             self.current = 1
@@ -88,14 +174,14 @@ class SobolSample:
 
     def generate_all_samples(self):
 
-        n_samples    = self.n_samples
-        n_dimensions = self.n_dimensions    
-        V = self.V 
+        N = self.N
+        D = self.D
+        V = self.V
 
-        sample_all = np.zeros([n_samples,n_dimensions])
+        sample_all = np.zeros([N,D])
 
         X = long(0)
-        for j in range(1, n_samples):
+        for j in range(1, N):
             X ^= V[self.index_of_least_significant_zero_bit(j - 1)]
             sample_all[j][:] = [float(x / math.pow(2, scale)) for x in X]
         return sample_all
